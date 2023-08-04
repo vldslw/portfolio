c410e1831da5793a5be726ed97c3e67193146c97
Vladislav Pavlov <vldslw@gmail.com>
2023-08-04 01:18:12 -0300

    feat: add header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,9 +1,16 @@
+import { Link } from 'react-router-dom';
 import './Header.css';
 
 export const Header = () => {
 
   return (
-		<div>
-    </div>
+    <header className="header">
+      <Link to="/" className="header__logo">vldslw</Link>
+      <nav className="header__nav">
+        <Link to="/" className="header__nav-link">About</Link>
+        <Link to="/" className="header__nav-link">Work</Link>
+        <Link to="/" className="header__nav-link">Contact</Link>
+      </nav>
+    </header>
   )
 };
