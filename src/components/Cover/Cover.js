75ccb77440bded04f34f77ee72be2c3bc43d9ffa
Vladislav Pavlov <vldslw@gmail.com>
2023-08-11 21:21:12 -0300

    feat: add cover section

--- src/components/Cover/Cover.js
+++ src/components/Cover/Cover.js
@@ -1,5 +1,6 @@
 import './Cover.css';
 import { Button } from '../Button/Button';
+import Photo from './vldslwPhoto.png';
 
 export const Cover = () => {
 
@@ -9,9 +10,22 @@ export const Cover = () => {
   }
 
   return (
-		<Button 
-    text={'My GitHub'}
-    handleClick={handleButtonClick}
-    />
+    <section className="cover">
+      <div className="cover__desc-block">
+        <h2 className="cover__desc-subhead">Web Development</h2>
+        <h1 className="cover__desc-head">Vladislav Pavlov</h1>
+        <p className="cover__desc-text">Frontend  Developer with experience in the media JavaScript | React</p>
+        <Button 
+         text={'My GitHub'}
+          handleClick={handleButtonClick}
+         />
+      </div>
+      <div className="cover__pic-block">
+        <img src={Photo} className="cover__pic" alt="Vladislav Pavlov"/>
+        <div className="cover__rec cover__rec_top"></div>
+        <div className="cover__rec cover__rec_bottom"></div>
+        <div className="cover__rotator"></div>
+      </div>
+    </section>
   )
 };
