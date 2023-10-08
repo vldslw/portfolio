e3e8287af5baa426293f9e0271111e094207389a
Vladislav Pavlov <vldslw@gmail.com>
2023-10-08 13:03:36 -0300

    feat: add vue icon to stack

--- src/components/Stack/Stack.js
+++ src/components/Stack/Stack.js
@@ -5,12 +5,14 @@ import node from './images/node.svg';
 import html from './images/html.svg';
 import css from './images/css.svg';
 import mongo from './images/mongo.svg';
+import vue from './images/vue.svg';
 
 export const Stack = () => {
 	return (
 		<section id="stack" className="stack">
 			<img src={javaScript} className="stack__icon" alt="JavaScript" />
 			<img src={react} className="stack__icon" alt="React" />
+			<img src={vue} className="stack__icon" alt="Vue.js" />
 			<img src={node} className="stack__icon" alt="Node.js" />
 			<img src={html} className="stack__icon" alt="HTML" />
 			<img src={css} className="stack__icon" alt="CSS" />
